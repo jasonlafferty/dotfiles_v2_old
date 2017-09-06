aa67d4ba3fad4da0a3ae9bcc175d0c3b68aac7ea
Jason Lafferty <jason_d_lafferty@hotmail.com>
2017-09-06 17:16:53 +0100

    Add setup file for flycheck

---
+++ site-lisp/setup-flycheck.el
@@ -0,0 +1,34 @@
+(use-package flycheck :ensure t
+  :diminish ""
+  :config
+
+  (setq flycheck-highlighting-mode 'lines)
+  (message "setup for flycheck working!!")
+  ;; flycheck for eslint
+  ;; https://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html#improving_the_jsx_syntax-hightlighting_in_web-mode
+  (defun my/eslint ()
+    (message "myeslint")
+    (setq-local flycheck-javascript-eslint-executable "/usr/local/bin/eslint"))
+  (add-hook 'flycheck-mode-hook #'my/eslint)
+
+
+  ;; Add better hightlighting for flycheck
+  ;; found https://github.com/flycheck/flycheck/issues/350
+  (eval-after-load 'flycheck
+    '(progn
+       (set-face-attribute 'flycheck-warning nil
+                           :foreground "yellow"
+                           :background "red"
+                           :underline "red")))
+
+  ;; disable jshint since we prefer eslint checking
+  (setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                      '(javascript-jshint)))
+
+  ;; use eslint with web-mode for jsx files
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+
+  (global-flycheck-mode))
+
+(provide 'setup-flycheck)
