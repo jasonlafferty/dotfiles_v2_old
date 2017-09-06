d549e316ef227948301011db32ce8885b7162bf6
Jason Lafferty <jason_d_lafferty@hotmail.com>
2017-09-06 17:17:15 +0100

    Add setup file for flyspell

---
+++ site-lisp/setup-flyspell.el
@@ -0,0 +1,23 @@
+(use-package flyspell :ensure t
+  :diminish "spell"
+  :defer t
+  :init
+  (add-hook 'prog-mode-hook #'flyspell-prog-mode)
+  (mapc (lambda (hook)
+          (add-hook hook #'flyspell-mode))
+        '(org-mode-hook
+          with-editor-mode-hook
+          rst-mode-hook))
+  :config
+  (setq flyspell-issue-message-flag nil)
+  (setq-default ispell-program-name "ispell")
+  (ispell-change-dictionary "english" t))
+
+(use-package helm-flyspell :ensure t
+  :after (helm flyspell)
+  :config
+  (general-define-key
+   :keymaps 'evil-normal-state-map
+   "z=" 'helm-flyspell-correct))
+
+(provide 'setup-flyspell)
